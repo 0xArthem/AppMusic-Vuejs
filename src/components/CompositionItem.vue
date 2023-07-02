0b8bb861406681a221f2c4e3321d6f7795fc96a2
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-07-02 18:25:02 +0200

    43 - Modification d'un son

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -16,6 +16,13 @@
       </button>
     </div>
     <div v-show="showForm">
+      <div
+        class="text-white text-center font-bold p-4 mb-4"
+        v-if="show_alert"
+        :class="alert_variant"
+      >
+        {{ alert_message }}
+      </div>
       <vee-form
         :validation-schema="schema"
         :initial-values="song"
@@ -42,10 +49,19 @@
           <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <div>
-          <button type="submit" class="py-1.5 px-3 rounded text-dark">
+          <button
+            type="submit"
+            class="py-1.5 px-3 rounded text-dark"
+            :disabled="in_submission"
+          >
             Submit
           </button>
-          <button type="button" class="py-1.5 px-3 rounded text-dark">
+          <button
+            type="button"
+            class="py-1.5 px-3 rounded text-dark"
+            :disabled="in_submission"
+            @click.prevent="showForm = false"
+          >
             Go Back
           </button>
         </div>
@@ -55,11 +71,21 @@
 </template>
 
 <script>
+import { songsCollection } from "@/includes/firebase";
+
 export default {
   name: "CompositionItem",
   props: {
     song: {
       type: Object,
+      required: true,
+    },
+    updateSong: {
+      type: Function,
+      required: true,
+    },
+    index: {
+      type: Number,
       required: true,
     },
   },
@@ -70,11 +96,32 @@ export default {
         modified_name: "required",
         genre: "alpha_spaces",
       },
+      in_submission: false,
+      show_alert: false,
+      alert_variant: "bg-blue-500",
+      alert_message: "Please wait !",
     };
   },
   methods: {
-    edit() {
-      console.log("Song edited.");
+    async edit(values) {
+      this.in_submission = true;
+      this.show_alert = true;
+      this.alert_variant = "bg-blue-500";
+      this.alert_message = "Please wait !";
+
+      try {
+        await songsCollection.doc(this.song.docID).update(values);
+      } catch (error) {
+        this.in_submission = false;
+        this.alert_variant = "bg-red-500";
+        this.alert_message = "Something went wrong !";
+      }
+
+      this.updateSong(this.index, values);
+
+      this.in_submission = false;
+      this.alert_variant = "bg-green-500";
+      this.alert_message = "Success !";
     },
   },
 };
