d43e3cfe2e1322ee446107f392b200cb9d1a489e
0xArthem <82900721+0xArthem@users.noreply.github.com>
2023-07-06 21:38:53 +0200

    60 - Playing audio

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,14 +1,20 @@
 import { defineStore } from "pinia";
+import { Howl } from "howler";
 
 export default defineStore("player", {
   actions: {
-    state: () => ({
-      currentSong: {},
-    }),
-    actions: {
-      async newSong(song) {
-        this.current_song = song;
-      },
+    newSong(song) {
+      this.currentSong = song;
+
+      this.sound = new Howl({
+        src: [song.url],
+        html5: true,
+        onloaderror: function () {
+          console.log("Erreur lors du chargement du fichier audio.");
+        },
+      });
+
+      this.sound.play();
     },
   },
 });
